f7453365f069408a77625643fb53e4a98347192c
Jahnviben <jahnvichaudhary796@gmail.com>
2026-09-02 19:10:29 +1000

    Implement view bookings

--- server/server.js
+++ server/server.js
@@ -1,32 +1,28 @@
 require("dotenv").config();
 
-// Import Express
 const express = require("express");
 
-// Create Express application
 const app = express();
 
-// Enable JSON request body parsing
 app.use(express.json());
 
-// Import authentication routes
 const authRoutes = require("./routes/authRoutes");
 
-// Import protected routes
 const protectedRoutes = require("./routes/protectedRoutes");
-
-// Mount authentication routes
-app.use("/api/auth", authRoutes);
-
-// Mount protected routes
-app.use("/api/protected", protectedRoutes);
-
-// Start the server
-const PORT = process.env.PORT || 5001;
 
 const festivalRoutes = require("./routes/festivalRoutes");
 
+const bookingRoutes = require("./routes/bookingRoutes");
+
+app.use("/api/auth", authRoutes);
+
+app.use("/api/protected", protectedRoutes);
+
 app.use("/api/festivals", festivalRoutes);
+
+app.use("/api/bookings", bookingRoutes);
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
